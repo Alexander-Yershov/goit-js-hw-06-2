0769900a2efc8a7bd0b857c5f567bb7432f68c20
Alexander-Yershov <alex04398956@gmail.com>
2022-10-16 17:04:41 +0200

    task8

---
+++ js/task-07.js
@@ -0,0 +1,5 @@
+const fontSizeControlRef = document.querySelector('#font-size-control');
+const spanTextRef = document.querySelector('#text');
+fontSizeControlRef.addEventListener('input', () => {
+    spanTextRef.style.fontSize = fontSizeControlRef.value + 'px'
+});
