e8b7d4399bab74dad063c1be50633f8615482c29
Alexander-Yershov <alex04398956@gmail.com>
2022-10-17 12:42:10 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
+const changeColorEl = document.querySelector('.change-color');
+const spanEl = document.querySelector('.color');
+changeColorEl.addEventListener('click', changeColor);
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function changeColor() {
+  const color = getRandomHexColor();
+  const bodyElem = document.querySelector('body');
+  bodyElem.style.backgroundColor = color;
+  spanEl.textContent = color;
 }
